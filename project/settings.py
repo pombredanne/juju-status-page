3125f486d0ebb9b30e3e2eb289f2ad20e3318f6c
Andrews Medina <andrewsmedina@gmail.com>
2013-01-31 11:17:29 -0200

    Removed database settings.

--- project/settings.py
+++ project/settings.py
@@ -12,30 +12,13 @@ ADMINS = ()
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
-
 TIME_ZONE = 'America/Chicago'
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
 MEDIA_ROOT = ''
 MEDIA_URL = ''
-
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ()
@@ -45,7 +28,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-SECRET_KEY = 'qnapn7923ao(-#fkw3)i(@@e=bf50mf#cf6v0)wlyr)#)-@50j'
+SECRET_KEY = 'ops'
 
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
@@ -65,30 +48,5 @@ TEMPLATE_DIRS = ()
 INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.staticfiles',
-
     'status',
 )
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
